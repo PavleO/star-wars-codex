65a775f16af0450132a25a0a6d98ebda165e5055
ozretic <pozretic@tripadvisor.com>
2023-02-21 20:31:49 +0100

    add pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/characters', pathMatch: 'full' },
+    {
+        path: 'characters',
+        loadChildren: () =>
+            import('./characters/characters.module').then(m => m.CharactersModule),
+    },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
---
+++ src/app/characters/characters-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CharactersListComponent } from 'src/app/characters/characters-list/characters-list.component';
+import { CharactersDetailComponent } from 'src/app/characters/characters-detail/characters-detail.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: CharactersListComponent,
+  },
+  {
+    path: ':id',
+    component: CharactersDetailComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(appRoutes)],
+  exports: [RouterModule],
+})
+export class CharactersRoutingModule {}
+
